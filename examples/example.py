faf8e9bf198bc5d7496cb52dd724111738881892
phileaton <philipeaton@mac.com>
2021-10-07 11:44:09 -0700

    Examples updated

--- examples/example.py
+++ examples/example.py
@@ -7,5 +7,10 @@ if __name__ == "__main__":
     account_id = os.environ["TRADIER_ACCOUNT_ID"]
     base_url = os.environ.get("TRADIER_BASE_URL")
     t = TradierAPI(token=token, default_account_id=account_id, base_url=base_url)
+
     profile = t.account.profile()
     print(profile.json())
+
+    balances = t.account.balances()
+    print(balances.json())
+
