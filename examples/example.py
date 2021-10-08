f643041434d478c375ef53b07b9ce0e123f908a0
Philip Eaton <philipeaton@mac.com>
2021-10-07 21:20:43 -0700

    Updated examples

--- examples/example.py
+++ examples/example.py
@@ -8,12 +8,33 @@ if __name__ == "__main__":
     base_url = os.environ.get("TRADIER_BASE_URL")
     t = TradierAPI(token=token, default_account_id=account_id, base_url=base_url)
 
-    # profile = t.account.profile()
-    # print(profile.json())
-    #
-    # balances = t.account.balances()
-    # print(balances.json())
-    #
+    profile = t.account.profile()
+    print(profile)
 
-    s = t.market.lookup_symbol("aapl")
-    print(s)
+    balances = t.account.balances()
+    print(balances)
+
+    orders = t.account.orders()
+    for o in orders:
+        print(o)
+
+    positions = t.account.positions()
+    for p in positions:
+        print(p)
+
+    symbol = "SPY"
+    side = "buy"
+    quantity = 1
+    order_type = "market"
+    duration = "day"
+
+    order = t.trading.order_equity(
+        symbol=symbol,
+        side=side,
+        quantity=quantity,
+        order_type=order_type,
+        duration=duration,
+    )
+
+    cancel_response = t.trading.cancel_order(order.id)
+    print(cancel_response)
