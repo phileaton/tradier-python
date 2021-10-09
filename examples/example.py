75b35ab0fb4962df0890535b2368a5b8e3aba8f0
Philip Eaton <philipeaton@mac.com>
2021-10-08 19:24:13 -0700

    Adding constants for endpoints

--- examples/example.py
+++ examples/example.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     token = os.environ["TRADIER_TOKEN"]
     account_id = os.environ["TRADIER_ACCOUNT_ID"]
     base_url = os.environ.get("TRADIER_BASE_URL")
-    t = TradierAPI(token=token, default_account_id=account_id, base_url=base_url)
+    t = TradierAPI(token=token, default_account_id=account_id, endpoint=base_url)
 
     profile = t.get_profile()
     print(profile)
